8c15c3048ff6f9296573dc6d832d29d70a29ad26
Bruno Staine <brunostaine@hotmail.com>
2023-12-19 10:06:09 -0300

    Criando a busca por recibo de estacionamento

--- src/main/java/com/brunostaine/api/park/services/ClienteVagaService.java
+++ src/main/java/com/brunostaine/api/park/services/ClienteVagaService.java
@@ -1,6 +1,7 @@
 package com.brunostaine.api.park.services;
 
 import com.brunostaine.api.park.entity.ClienteVaga;
+import com.brunostaine.api.park.exceptions.EntityNotFoundException;
 import com.brunostaine.api.park.repository.ClienteVagaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,4 +16,12 @@ public class ClienteVagaService {
     public ClienteVaga salvar(ClienteVaga clienteVaga) {
         return clienteVagaRepository.save(clienteVaga);
     }
+
+    @Transactional(readOnly = true)
+    public ClienteVaga buscarPorRecibo(String recibo) {
+        return clienteVagaRepository.findByReciboAndDataSaidaIsNull(recibo).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Recibo '%s' não encontrado no sistema ou check-out já realizado", recibo))
+        );
+    }
+
 }
