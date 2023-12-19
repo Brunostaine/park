5d221e52e2f3e6c7fed8ecdb3003aea423e09462
Bruno Staine <brunostaine@hotmail.com>
2023-12-19 11:00:06 -0300

    Adicionando testes de checkout

--- src/main/java/com/brunostaine/api/park/web/controller/EstacionamentoController.java
+++ src/main/java/com/brunostaine/api/park/web/controller/EstacionamentoController.java
@@ -5,10 +5,11 @@ import com.brunostaine.api.park.services.ClienteVagaService;
 import com.brunostaine.api.park.services.EstacionamentoService;
 import com.brunostaine.api.park.web.dto.EstacionamentoCreateDTO;
 import com.brunostaine.api.park.web.dto.EstacionamentoResponseDTO;
-import com.brunostaine.api.park.web.dto.UsuarioResponseDTO;
 import com.brunostaine.api.park.web.dto.mapper.ClienteVagaMapper;
 import com.brunostaine.api.park.web.exceptions.ErrorMessage;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+
 @Tag(name = "Estacionamentos", description = "Operações de registros de entrada e saída de um veículo do estacionamento")
 @RequiredArgsConstructor
 @RestController
@@ -37,26 +39,26 @@ public class EstacionamentoController {
             "Requisição exige um Bearer Token. Acesso restrito a ADMIN | CLIENTE",
             security = @SecurityRequirement(name = "security"),
             responses = {
-            @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
-                    headers = @Header(name = HttpHeaders.LOCATION, description = "URL de acesso ao recurso criado"),
-                    content = @Content(mediaType = "application/json;charset=UTF=8",
-                        schema = @Schema(implementation = EstacionamentoResponseDTO.class))),
-            @ApiResponse(responseCode = "403", description = "Recurso não permitido ao perfil de CLIENTE",
-                    content = @Content(mediaType = "application/json;charset=UTF=8",
-                        schema = @Schema(implementation = ErrorMessage.class))),
-            @ApiResponse(responseCode = "404", description = "Causas Possíveis: <br/>" +
-                    "- CPF do cliente não cadastrado no sistema; <br/>" +
-                    "- Nenhuma vaga livre foi localizada;",
-                    content = @Content(mediaType = "application/json;charset=UTF=8",
-                        schema = @Schema(implementation = ErrorMessage.class))),
-            @ApiResponse(responseCode = "422", description = "Recurso não processado por falta de dados ou dados inválidos",
-                    content = @Content(mediaType = "application/json;charset=UTF=8",
-                            schema = @Schema(implementation = ErrorMessage.class)))
-    })
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                            headers = @Header(name = HttpHeaders.LOCATION, description = "URL de acesso ao recurso criado"),
+                            content = @Content(mediaType = "application/json;charset=UTF=8",
+                                    schema = @Schema(implementation = EstacionamentoResponseDTO.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permitido ao perfil de CLIENTE",
+                            content = @Content(mediaType = "application/json;charset=UTF=8",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Causas Possíveis: <br/>" +
+                            "- CPF do cliente não cadastrado no sistema; <br/>" +
+                            "- Nenhuma vaga livre foi localizada;",
+                            content = @Content(mediaType = "application/json;charset=UTF=8",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por falta de dados ou dados inválidos",
+                            content = @Content(mediaType = "application/json;charset=UTF=8",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            })
 
     @PostMapping("/check-in")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<EstacionamentoResponseDTO> checkin(@RequestBody @Valid EstacionamentoCreateDTO dto){
+    public ResponseEntity<EstacionamentoResponseDTO> checkin(@RequestBody @Valid EstacionamentoCreateDTO dto) {
         ClienteVaga clienteVaga = ClienteVagaMapper.toClienteVaga(dto);
         estacionamentoService.checkIn(clienteVaga);
         EstacionamentoResponseDTO responseDTO = ClienteVagaMapper.toDto(clienteVaga);
@@ -66,6 +68,21 @@ public class EstacionamentoController {
                 .toUri();
         return ResponseEntity.created(location).body(responseDTO);
     }
+
+    @Operation(summary = "Localizar um veículo estacionado", description = "Recurso para retornar um veículo estacionado " +
+            "pelo nº do recibo. Requisição exige uso de um bearer token.",
+            security = @SecurityRequirement(name = "security"),
+            parameters = {
+                    @Parameter(in = ParameterIn.PATH, name = "recibo", description = "Número do rebibo gerado pelo check-in")
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso localizado com sucesso",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = EstacionamentoResponseDTO.class))),
+                    @ApiResponse(responseCode = "404", description = "Número do recibo não encontrado.",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @GetMapping("/check-in/{recibo}")
     @PreAuthorize("hasAnyRole('ADMIN', 'CLIENTE')")
     public ResponseEntity<EstacionamentoResponseDTO> getByRecibo(@PathVariable String recibo) {
@@ -74,6 +91,24 @@ public class EstacionamentoController {
         return ResponseEntity.ok(dto);
     }
 
+    @Operation(summary = "Operação de check-out", description = "Recurso para dar saída de um veículo do estacionamento. " +
+            "Requisição exige uso de um bearer token. Acesso restrito a Role='ADMIN'",
+            security = @SecurityRequirement(name = "security"),
+            parameters = {@Parameter(in = ParameterIn.PATH, name = "recibo", description = "Número do rebibo gerado pelo check-in",
+                    required = true)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso atualzado com sucesso",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = EstacionamentoResponseDTO.class))),
+                    @ApiResponse(responseCode = "404", description = "Número do recibo inexistente ou " +
+                            "o veículo já passou pelo check-out.",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permito ao perfil de CLIENTE",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PutMapping("/check-out/{recibo}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<EstacionamentoResponseDTO> checkout(@PathVariable String recibo) {
